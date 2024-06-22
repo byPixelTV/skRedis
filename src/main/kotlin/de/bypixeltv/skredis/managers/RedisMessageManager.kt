42e8a980e376622841b6f377b165e5201ad17c76
Lukas <113579672+byPixelTV@users.noreply.github.com>
2024-06-22 12:50:47 +0200

    Removed some log messages

--- src/main/kotlin/de/bypixeltv/skredis/managers/RedisMessageManager.kt
+++ src/main/kotlin/de/bypixeltv/skredis/managers/RedisMessageManager.kt
@@ -9,22 +9,18 @@ object RedisMessageManager {
     init {
         val jedisPubSub = object : JedisPubSub() {
             override fun onPMessage(pattern: String, channel: String, message: String) {
-                Main.INSTANCE.sendLogs("onPMessage called with pattern: $pattern, channel: $channel and message: $message")
                 Main.INSTANCE.getRC()?.processMessage(channel, message)
             }
 
             override fun onPSubscribe(pattern: String, subscribedChannels: Int) {
-                Main.INSTANCE.sendLogs("onPSubscribe called with pattern: $pattern and subscribedChannels: $subscribedChannels")
                 super.onPSubscribe(pattern, subscribedChannels)
             }
         }
 
         // Run the subscription in a new thread
         Thread {
-            Main.INSTANCE.sendLogs("Starting subscription thread...")
             try {
                 redis?.resource?.use { jedis ->
-                    Main.INSTANCE.sendLogs("Subscribing to Redis...")
                     jedis.psubscribe(jedisPubSub, "*")
                 }
             } catch (e: Exception) {
