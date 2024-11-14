c5f29b8ef0f3ee418108e128a208b8a88488c68c
Lukas <113579672+byPixelTV@users.noreply.github.com>
2024-11-14 15:32:28 +0100

    chore: update spigot api and other dependencies + did some project cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.0.21"
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
+val versionString = "1.0.5"
+
 group = "de.bypixeltv"
-version = "1.0.4"
+version = versionString
 
 repositories {
     mavenCentral()
@@ -19,15 +23,17 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.21.3-R0.1-SNAPSHOT")
 
-    implementation("net.kyori:adventure-platform-bukkit:4.3.4")
-    implementation("net.kyori:adventure-text-minimessage:4.17.0")
-    implementation("net.axay:kspigot:1.21.0")
+    bukkitLibrary("net.kyori:adventure-platform-bukkit:4.3.4")
+    bukkitLibrary("net.kyori:adventure-text-minimessage:4.17.0")
+    bukkitLibrary("net.axay:kspigot:1.21.0")
 
-    implementation("com.github.SkriptLang:Skript:2.9.3")
+    compileOnly("com.github.SkriptLang:Skript:2.9.4")
 
-    implementation("redis.clients:jedis:5.2.0")
+    bukkitLibrary("redis.clients:jedis:5.2.0")
+
+    implementation("com.github.technicallycoded:FoliaLib:main-SNAPSHOT")
 }
 
 sourceSets {
@@ -46,7 +52,44 @@ tasks {
         options.encoding = "UTF-8"
         options.release.set(21)
     }
+
     named("compileKotlin", org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask::class.java)
+
+    shadowJar {
+        relocate("com.tcoded.folialib", "de.bypixeltv.skredis.lib.folialib")
+
+        archiveBaseName.set("SkRedis")
+        archiveVersion.set(version.toString())
+        archiveClassifier.set("")
+    }
+}
+
+bukkit {
+    main = "de.bypixeltv.skredis.Main"
+
+    version = versionString
+
+    foliaSupported = true
+
+    apiVersion = "1.13"
+
+    authors = listOf("byPixelTV")
+
+    website = "https://bypixeltv.de"
+
+    description = "The modern way to use Redis with Skript."
+
+    depend = listOf("Skript")
+
+    prefix = "SkRedis"
+
+    commands {
+        register("skredis") {
+            description = "Main command for SkRedis"
+            permission = "skredis.admin"
+            usage = "/skredis <info|version|reload|reloadredis>"
+        }
+    }
 }
 
 kotlin {
